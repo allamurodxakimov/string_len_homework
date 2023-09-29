275c193a021bcd4cebe6a9b2f1338a07d2571335
allamurodxakimov <xakimov2806@gmail.com>
2023-09-29 15:58:19 -0700

    string len 7

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,22 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    a = len(s1)
+    b = len(s2)
+    c = len(s3)
+    if  a%2 == 0 and b%2==1 and c%2==1:
+        return f"[{s3},{s2}]"
+    elif a%2 != 0 and b%2==0 and c%2==0:
+        return f"[{s1}]"
+    if  b%2 == 0 and a%2==1 and c%2==1:
+        return f"[{s1},{s3}]"
+    elif b%2 == 1 and b%2==0 and c%2==0 :
+        return f"[{s2}]"
+    if  c%2 == 0 and b%2==1 and a%2==1:
+        return f"[{s1},{s2}]"
+    elif  c%2 == 1 and b%2==0 and a%2==0:
+        return f"[{s3}]"
+    
+        
+print(main( s1="code", s2="python", s3="coder"))
+print(main(s1="codeschool.uz" ,s2="example" ,s3="python"))
